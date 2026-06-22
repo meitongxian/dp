8b253512039e6f79b2812f288caed432994c3235
Maggie Xian <111546244+meitongxian@users.noreply.github.com>
2026-06-22 16:22:39 -0400

    add image hover, tweaks

---
+++ images.js
@@ -0,0 +1,16 @@
+// // images on hover
+
+let indexLinks = document.querySelectorAll(".index-item > a");
+let imgWrapper = document.querySelector(".img-wrapper");
+let img = document.querySelector(".img-wrapper > img");
+
+for (let i = 0; i < 11; i++) {
+    indexLinks[i].addEventListener("mouseenter", () => {
+        // console.log("hovered");
+        imgWrapper.classList.add("img-wrapper_active");
+        img.src = `images/` + (i+1) + `.png`;
+    });
+    indexLinks[i].addEventListener("mouseleave", () => {
+        imgWrapper.classList.remove("img-wrapper_active");
+    });
+}
